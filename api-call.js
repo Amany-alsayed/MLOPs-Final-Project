4876231f4c26b4dfd4c832bef0bcb90c15c7fd86
Amany-alsayed <159948094+Amany-alsayed@users.noreply.github.com>
2025-06-01 16:47:24 +0300

    "add api"

--- api-call.js
+++ api-call.js
@@ -1,23 +1,30 @@
 async function getPredictedLabel(processed_t) {
   try {
+    //console.log("Is processed_t an array?", Array.isArray(processed_t));
+    //console.log("Is processed_t flat?", processed_t.every(x => typeof x === "number"));
+    //console.log("Is processed_t flat?", processed_t);
+    processed_t = processed_t.flatMap(point => [point.x, point.y, point.z]);
+    //console.log("Is processed_t an array?", Array.isArray(processed_t));
+    //console.log("Is processed_t flat?", processed_t.every(Number.isFinite)); // should be true after flatten
+    //console.log("Is processed_t flat?", processed_t);
     const response = await fetch("http://localhost:8000/predict", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        landmarks: processed_t  
+        landmarks: processed_t
       })
     });
     if (!response.ok) {
       throw new Error("Prediction request failed");
     }
     const result = await response.json();
-    console.log("Predicted label:", result.label);
-    return result.label;
+    console.log("Predicted label:", result.prediction);
+    return result.prediction;
   } catch (error) {
     console.error("Error calling prediction API:", error);
-    return null; // fallback to no movement
+    return null;
   }
 }
 
